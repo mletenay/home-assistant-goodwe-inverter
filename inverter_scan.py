0738281569f334391864a85be28e7b01eceda0c6
mletenay <mle@whitestein.com>
2021-01-19 23:22:14 +0100

    Add serialNr to omnik protocol attempts

--- inverter_scan.py
+++ inverter_scan.py
@@ -28,8 +28,9 @@ def omnik_command(logger_sn):
     defchk = binascii.unhexlify('87')
     endCode = binascii.unhexlify('16')
 
-    tar = bytearray.fromhex(hex(logger_sn)[8:10] + hex(logger_sn)[6:8] + hex(logger_sn)[4:6] + hex(logger_sn)[2:4])
-    frame = bytearray(frame_hdr + tar + tar + command + defchk + endCode)
+    #tar = bytearray.fromhex(hex(logger_sn)[8:10] + hex(logger_sn)[6:8] + hex(logger_sn)[4:6] + hex(logger_sn)[2:4])
+    #frame = bytearray(frame_hdr + tar + tar + command + defchk + endCode)
+    frame = bytearray(frame_hdr + binascii.unhexlify(logger_sn) + command + defchk + endCode)
 
     checksum = 0
     frame_bytes = bytearray(frame)
@@ -48,8 +49,12 @@ try_command("AA55C07F0102000241", result[0])
 try_command("7F03753100280409", result[0])
 # Omnik v5 ?
 try_command("197d0001000dff045e50303036564657f6e60d", result[0])
-# Omnik 4 (no serial) ?
-try_command(omnik_command(12345678), result[0])
+# Omnik 4 ?
+sn = bytes(result[2][10:], 'utf-8').hex()
+try_command(omnik_command(sn), result[0])
+# Omnik 4 reversed ?
+sn = "".join(reversed([sn[i:i+2] for i in range(0, len(sn), 2)]))
+try_command(omnik_command(sn), result[0])
 
 
 print(f"Identifying inverter at IP: {result[0]}")
