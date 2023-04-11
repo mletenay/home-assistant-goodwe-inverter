bb8a17aabf6582cfe04cdedfcb57f787a0378677
mletenay <mle@whitestein.com>
2023-04-12 00:09:49 +0200

    Updated scan/test helper scripts

--- inverter_scan.py
+++ inverter_scan.py
@@ -1,6 +1,5 @@
 """Simple test script to scan inverter present on local network"""
 import asyncio
-import binascii
 import goodwe
 import logging
 import sys
@@ -8,55 +7,12 @@ import sys
 logging.basicConfig(
     format="%(asctime)-15s %(funcName)s(%(lineno)d) - %(levelname)s: %(message)s",
     stream=sys.stderr,
-    level=getattr(logging, "DEBUG", None),
+    level=getattr(logging, "ERROR", None),
 )
-
-def try_command(command, ip):
-    print(f"Trying command: {command}")
-    try:
-        response = asyncio.run(goodwe.ProtocolCommand(bytes.fromhex(command), lambda x: True).execute(result[0]))
-        print(f"Response to {command} command: {response.hex()}")
-    except Exception as err:
-        print(f"No response to {command} command")
-
-
-def omnik_command(logger_sn):
-    #frame = (headCode) + (dataFieldLength) + (contrlCode) + (sn) + (sn) + (command) + (checksum) + (endCode)
-    frame_hdr = binascii.unhexlify('680241b1') #from SolarMan / new Omnik app
-    command = binascii.unhexlify('0100')
-    defchk = binascii.unhexlify('87')
-    endCode = binascii.unhexlify('16')
-
-    #tar = bytearray.fromhex(hex(logger_sn)[8:10] + hex(logger_sn)[6:8] + hex(logger_sn)[4:6] + hex(logger_sn)[2:4])
-    #frame = bytearray(frame_hdr + tar + tar + command + defchk + endCode)
-    frame = bytearray(frame_hdr + binascii.unhexlify(logger_sn) + command + defchk + endCode)
-
-    checksum = 0
-    frame_bytes = bytearray(frame)
-    for i in range(1, len(frame_bytes) - 2, 1):
-        checksum += frame_bytes[i] & 255
-    frame_bytes[len(frame_bytes) - 2] = int((checksum & 255))
-    return frame_bytes.hex()
 
 result = asyncio.run(goodwe.search_inverters()).decode("utf-8").split(",")
 print(f"Located inverter at IP: {result[0]}, mac: {result[1]}, name: {result[2]}")
 
-
-# EM/ES
-try_command("AA55C07F0102000241", result[0])
-# DT (SolarGo)
-try_command("7F03753100280409", result[0])
-# Omnik v5 ?
-try_command("197d0001000dff045e50303036564657f6e60d", result[0])
-# Omnik 4 ?
-sn = bytes(result[2][10:], 'utf-8').hex()
-try_command(omnik_command(sn), result[0])
-# Omnik 4 reversed ?
-sn = "".join(reversed([sn[i:i+2] for i in range(0, len(sn), 2)]))
-try_command(omnik_command(sn), result[0])
-
-
-print(f"Identifying inverter at IP: {result[0]}")
 inverter = asyncio.run(goodwe.discover(result[0], 8899))
 print(
     f"Identified inverter model: {inverter.model_name}, serialNr: {inverter.serial_number}"
--- inverter_test.py
+++ inverter_test.py
@@ -8,35 +8,30 @@ import sys
 logging.basicConfig(
     format="%(asctime)-15s %(funcName)s(%(lineno)d) - %(levelname)s: %(message)s",
     stream=sys.stderr,
-    level=getattr(logging, "DEBUG", None),
+    level=getattr(logging, "ERROR", None),
 )
 
 # Set the appropriate IP address
 IP_ADDRESS = "192.168.1.14"
 
-FAMILY = "ET"  # One of ET, EH, ES, EM, DT, NS, XS, BP or None to detect inverter family automatically
-COMM_ADDR = None  # Usually 0xf7 for ET/EH or 0x7f for DT/D-NS/XS, or None for default value
+FAMILY = "ET"  # One of ET, ES, DT or None to detect inverter family automatically
+COMM_ADDR = None  # Usually 0xf7 for ET/ES or 0x7f for DT, or None for default value
 TIMEOUT = 1
 RETRIES = 3
 
 inverter = asyncio.run(goodwe.connect(IP_ADDRESS, COMM_ADDR, FAMILY, TIMEOUT, RETRIES))
 print(
-    f"Identified inverter\n"
-    f"- Model: {inverter.model_name}\n"
-    f"- SerialNr: {inverter.serial_number}\n"
-    f"- Version: {inverter.software_version}"
+    f"Identified inverter:\n"
+    f"\tModel:    {inverter.model_name}\n"
+    f"\tSerialNr: {inverter.serial_number}\n"
+    f"\tFirmware: {inverter.firmware}"
 )
 
 response = asyncio.run(inverter.read_runtime_data())
 
+print("\nSensors values:")
 for sensor in inverter.sensors():
     if sensor.id_ in response:
-        print(f"{sensor.id_}: \t\t {sensor.name} = {response[sensor.id_]} {sensor.unit}")
-
-# response = asyncio.run(inverter.read_settings_data())
-
-# for setting in inverter.settings():
-#    print(f"{setting.id_}: \t\t {setting.name} = {response[setting.id_]} {setting.unit}")
-
-# response = asyncio.run(goodwe.protocol.ModbusReadCommand(0xf7, 0x88b8, 1).execute(IP_ADDRESS, TIMEOUT, RETRIES))
-# print(response.hex())
+        print(
+            f"\t{sensor.id_:30}:\t{sensor.name} = {response[sensor.id_]} {sensor.unit}"
+        )
