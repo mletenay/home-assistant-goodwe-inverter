a324632bb1b3a7ebe40c8e13b3d9e43c694ac71e
mletenay <mle@whitestein.com>
2023-12-12 23:25:28 +0100

    Add extended sensors to ETT inverters

--- custom_components/goodwe/diagnostics.py
+++ custom_components/goodwe/diagnostics.py
@@ -1,27 +1,27 @@
 """Diagnostics support for Goodwe."""
 from __future__ import annotations
 
-from dataclasses import asdict
 from typing import Any
+
+from goodwe import Inverter
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from . import GoodweUpdateCoordinator
-from .const import DOMAIN, KEY_DEVICE_INFO, KEY_INVERTER
+from .const import DOMAIN, KEY_INVERTER
 
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    inverter = hass.data[DOMAIN][config_entry.entry_id][KEY_INVERTER]
-    device_info = hass.data[DOMAIN][config_entry.entry_id][KEY_DEVICE_INFO]
+    inverter: Inverter = hass.data[DOMAIN][config_entry.entry_id][KEY_INVERTER]
 
     diagnostics_data = {
         "config_entry": config_entry.as_dict(),
         "inverter": {
             "model_name": inverter.model_name,
+            "rated_power": inverter.rated_power,
             "firmware": inverter.firmware,
             "arm_firmware": inverter.arm_firmware,
             "dsp1_version": inverter.dsp1_version,
